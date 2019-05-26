d9166d573b3b131015b1e3ad14e19e91f1439196
Gabriel Araujo <gabriel-araujo52@hotmail.com>
2019-05-26 19:28:15 -0300

    quase pronto, só  que não

--- Assembler.py
+++ Assembler.py
@@ -22,121 +22,94 @@ def abrirArquivo():
     return {'k':k,'d':d,'sequencia':ls}
 
 def prefixo(i):
-    s1 , s2 = i['rotulo'].split('|')
+    s1 , s2 = i.split('|')
     s1 = s1[0:-1]
     s2 = s2[0:-1]
     return (s1, s2)
 
 def sufixo(i):
-    s1 , s2 = i['rotulo'].split('|')
+    s1 , s2 = i.split('|')
     s1 = s1[1:]
     s2 = s2[1:]
     return (s1, s2)
 
 def geraAdjLista(composicao):
-    grafo = []
+    grafo = {}
+    saida = {}
+    entrada = {}
     for x in composicao['sequencia']:
-        grafo.append({'rotulo':x, 'adj':[], 'ant':[], 'saida':0, 'entrada':0})
-    tam = len(grafo)
-    ini = []
-    for i in range(tam):
-        for j in range(tam):
-            if j==i:
-                continue 
-            if prefixo(grafo[j]) == sufixo(grafo[i]):
-                grafo[i]['adj'].append(j)
-                grafo[i]['saida'] += 1
-            if prefixo(grafo[i]) == sufixo(grafo[j]):
-                grafo[i]['ant'].append(j)
-                grafo[i]['entrada'] += 1
-    return grafo
+        grafo[prefixo(x)] = []
+        saida[prefixo(x)] = 0
+        saida[sufixo(x)] = 0
+        entrada[sufixo(x)] = 0
+        entrada[prefixo(x)] = 0
+    for x in composicao['sequencia']:
+        grafo[prefixo(x)].append(sufixo(x))
+        saida[prefixo(x)] += 1
+        entrada[sufixo(x)] += 1
+    return [grafo,saida,entrada]
 
-def encontraInicio(grafo):
-    tam = len(grafo)
-    inicio = -1
-    for i in range(tam):
-        if grafo[i]['ant'] == []:
-            inicio = i
-    return inicio
+def encontraInicio(entrada, saida):
+    mim = 0
+    chave = list(entrada)[0]
+    for i in (entrada):
+        if entrada[i] - saida[i] <= mim:
+            mim = entrada[i] - saida[i]
+            chave = i
+    return chave
 
-def remonta(s, d):
-    tam = len(s)
-    se = ""
-    for i in range(tam):
-        ini = grafo[s[i]]['rotulo'].split('|')[0]
-        fin = grafo[s[i]]['rotulo'].split('|')[1]
-        if se == "":
-            se += ini
-        else:
-            se += ini[-1]
-        if i==tam-1:
-            se += grafo[s[i-d]]['rotulo'].split('|')[1][:d]
-            se += fin
-    return se
-
-
-def gerasequencia(i, sequencia):
-    ini = grafo[i]['rotulo'].split('|')[0]
-    if sequencia == "":
-        sequencia += ini
-    else:
-        sequencia += ini[-1]
-    adjlist = grafo[i]['adj']
-    if adjlist != []:
-        for prox in adjlist:
-            gerasequencia(prox, sequencia)
-    else:
-        sequencia += grafo[i]['rotulo'].split('|')[1]
-        print(sequencia)
-
-def geraCaminho(grafo, inicio):
-    if inicio == -1:
-        inicio = grafo[inicio]['adj'].pop()
-    if grafo[inicio]['adj'] == []:
-        return [inicio]
-    return [inicio]+geraCaminho(grafo, grafo[inicio]['adj'].pop())
-
-def acha_caminho(grafo, v):
+def acha_caminho(grafo, entrada, saida, chave):
     caminho = []
     pilha = []
-    if v == -1:
-        v = 0
     while True:
-        print("pi",pilha)
-        print("cam",caminho)
-        saida = grafo[v]['saida']
-        if len(pilha) == 0 and saida == 0:
-            caminho = [v] + caminho
+        if len(pilha) == 0 and saida[chave] == 0:
+            caminho.append(chave)
             break
         else:
-            if saida == 0:
-                caminho = [v] + caminho
-                v = pilha.pop()
+            if saida[chave] == 0:
+                caminho.append(chave)
+                chave = pilha.pop()
             else:
-                pilha.append(v)
-                grafo[v]['saida'] = -1
-                v = grafo[v]['adj'].pop()
-                grafo[v]['entrada'] = -1
-            
-    return caminho
+                pilha.append(chave)
+                viz = grafo[chave].pop()
+                saida[chave] +=-1
+                entrada[viz] +=-1
+                chave = viz
+    return caminho[::-1]
 
+def remonta(k, caminho):
+    sequencia = ""
+    for i in caminho:
+        if sequencia == "":
+            sequencia += i[0][0] 
+        else:
+            sequencia += i[0][0]
     
+    return sequencia + caminho[-1][0][1:] + caminho[-k][1][:k]  + caminho[-1][1]
+
 
 composicao = abrirArquivo()
-grafo = geraAdjLista(composicao)
-inicio = encontraInicio(grafo)
-for g in grafo:
-    print(g['adj'])
+grafo, saida, entrada = geraAdjLista(composicao)
+chave_inicio = encontraInicio(entrada, saida)
+#print(chave_inicio)
+cami = acha_caminho(grafo, entrada, saida, chave_inicio)
+print(cami)
+sequencia = remonta(composicao['k'],cami)
+print(sequencia)
+#for g in grafo:
+#    print(g['adj'])
 #gerasequencia(inicio,"")
 #novo_grafo = deepcopy(grafo)
 #seq = geraCaminho(novo_grafo, inicio)
-print(grafo)
-print(inicio)
+#print(grafo)
+#print(saida)
+#print(entrada)
+#print(inicio)
 
-cami = acha_caminho(grafo, inicio)
-print(cami)
-se = remonta(cami, composicao['d'])
-print(se)
+
+#print(cami)
+#se = remonta(cami, composicao['d'])
+#print(se)
 
 
 
