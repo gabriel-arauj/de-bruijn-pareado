c6d3c499fb7352624b658d922bd8b738873e4417
Gabriel Araujo <gabriel-araujo52@hotmail.com>
2019-05-26 16:34:36 -0300

    acha caminho euleriano ok eu acho

--- Assembler.py
+++ Assembler.py
@@ -1,4 +1,6 @@
 import sys
+from copy import deepcopy
+
 
 # Abrindo arquivo kdmer
 def abrirArquivo():
@@ -34,7 +36,7 @@ def sufixo(i):
 def geraAdjLista(composicao):
     grafo = []
     for x in composicao['sequencia']:
-        grafo.append({'rotulo':x, 'adjlist':[], 'ant':[]})
+        grafo.append({'rotulo':x, 'adj':[], 'ant':[], 'saida':0, 'entrada':0})
     tam = len(grafo)
     ini = []
     for i in range(tam):
@@ -42,24 +44,12 @@ def geraAdjLista(composicao):
             if j==i:
                 continue 
             if prefixo(grafo[j]) == sufixo(grafo[i]):
-                grafo[i]['adjlist'].append(j)
+                grafo[i]['adj'].append(j)
+                grafo[i]['saida'] += 1
             if prefixo(grafo[i]) == sufixo(grafo[j]):
                 grafo[i]['ant'].append(j)
+                grafo[i]['entrada'] += 1
     return grafo
-
-def gerasequencia(i, sequencia):
-    ini = grafo[i]['rotulo'].split('|')[0]
-    if sequencia == "":
-        sequencia += ini
-    else:
-        sequencia += ini[-1]
-    adjlist = grafo[i]['adjlist']
-    if adjlist != []:
-        for prox in adjlist:
-            gerasequencia(prox, sequencia)
-    else:
-        sequencia += grafo[i]['rotulo'].split('|')[1]
-        print(sequencia)
 
 def encontraInicio(grafo):
     tam = len(grafo)
@@ -69,14 +59,7 @@ def encontraInicio(grafo):
             inicio = i
     return inicio
 
-def geraCaminho(grafo, inicio):
-    if inicio == -1:
-        inicio = grafo[inicio]['adjlist'].pop()
-    if grafo[inicio]['adjlist'] == []:
-        return [inicio]
-    return [inicio]+geraCaminho(grafo, grafo[inicio]['adjlist'].pop())
-
-def sequencia(s, d):
+def remonta(s, d):
     tam = len(s)
     se = ""
     for i in range(tam):
@@ -91,14 +74,68 @@ def sequencia(s, d):
             se += fin
     return se
 
+
+def gerasequencia(i, sequencia):
+    ini = grafo[i]['rotulo'].split('|')[0]
+    if sequencia == "":
+        sequencia += ini
+    else:
+        sequencia += ini[-1]
+    adjlist = grafo[i]['adj']
+    if adjlist != []:
+        for prox in adjlist:
+            gerasequencia(prox, sequencia)
+    else:
+        sequencia += grafo[i]['rotulo'].split('|')[1]
+        print(sequencia)
+
+def geraCaminho(grafo, inicio):
+    if inicio == -1:
+        inicio = grafo[inicio]['adj'].pop()
+    if grafo[inicio]['adj'] == []:
+        return [inicio]
+    return [inicio]+geraCaminho(grafo, grafo[inicio]['adj'].pop())
+
+def acha_caminho(grafo, v):
+    caminho = []
+    pilha = []
+    if v == -1:
+        v = 0
+    while True:
+        print("pi",pilha)
+        print("cam",caminho)
+        saida = grafo[v]['saida']
+        if len(pilha) == 0 and saida == 0:
+            caminho = [v] + caminho
+            break
+        else:
+            if saida == 0:
+                caminho = [v] + caminho
+                v = pilha.pop()
+            else:
+                pilha.append(v)
+                grafo[v]['saida'] = -1
+                v = grafo[v]['adj'].pop()
+                grafo[v]['entrada'] = -1
+            
+    return caminho
+
+    
+
 composicao = abrirArquivo()
 grafo = geraAdjLista(composicao)
 inicio = encontraInicio(grafo)
-#print(grafo)
-gerasequencia(inicio,"")
-seq = geraCaminho(grafo, inicio)
-print(seq)
-se = sequencia(seq, composicao['d'])
+for g in grafo:
+    print(g['adj'])
+#gerasequencia(inicio,"")
+#novo_grafo = deepcopy(grafo)
+#seq = geraCaminho(novo_grafo, inicio)
+print(grafo)
+print(inicio)
+
+cami = acha_caminho(grafo, inicio)
+print(cami)
+se = remonta(cami, composicao['d'])
 print(se)
 
 
